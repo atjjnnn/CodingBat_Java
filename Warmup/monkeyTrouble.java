38a3d6fe13a5868a00b5cc1d998ae51307a268fc
atjjnnn <50092173+atjjnnn@users.noreply.github.com>
2022-10-08 21:08:36 +0900

    add: add a new file

---
+++ Warmup/monkeyTrouble.java
@@ -0,0 +1,11 @@
+public boolean monkeyTrouble(boolean aSmaile, boolean bSmile) {
+	if (aSmile && bSmile) {
+		return true;
+	}
+	if (!aSmile && !bSmile) {
+		return ture;
+	}
+	return false;
+
+//	return (aSmile == bSmile);
+}
